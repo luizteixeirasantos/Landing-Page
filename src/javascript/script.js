8e9f43ce4876c44fc0920a6f9ed3d2a15583dbba
Luiz <luiz.hh200@gmail.com>
2025-12-08 23:22:47 -0300

    ;

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -97,3 +97,17 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll(".title, .description").forEach((el) => {
   observer.observe(el);
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+
+const mobileMenuLinks = document.querySelectorAll(
+  '#menu-mobile-links a[href^="#"]'
+);
+
+mobileMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (menuToggle) {
+      menuToggle.checked = false;
+    }
+  });
+});
